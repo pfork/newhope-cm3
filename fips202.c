faa9b1296bedee8d4eee733614daea2132b1134d
stf <7o5rfu92t@ctrlc.hu>
2017-03-11 17:18:18 +0000

    [mod] removed debug asserts

--- fips202.c
+++ fips202.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include <assert.h> //XXX: only for debugging
+//#include <assert.h> //XXX: only for debugging
 #include "fips202.h"
 #include <string.h>
 
@@ -53,7 +53,7 @@ void shake128_squeezeblocks(unsigned char *output, unsigned long long nblocks, u
 void shake128(unsigned char *output, unsigned int outputByteLen, const unsigned char *input, unsigned int inputByteLen)
 {
   uint64_t s[25];
-  assert(!(outputByteLen%SHAKE128_RATE));
+  //assert(!(outputByteLen%SHAKE128_RATE));
   shake128_absorb(s, input, inputByteLen);
   shake128_squeezeblocks(output, outputByteLen/SHAKE128_RATE, s);
 }
